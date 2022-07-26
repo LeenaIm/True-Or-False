314a4f80f33ef683dd72b70c61b5c3ed34d374a9
LeenaIm <99370611+LeenaIm@users.noreply.github.com>
2022-07-26 16:12:52 +1200

    Add files via upload

--- 01_TF_Base.py
+++ 01_TF_Base.py
@@ -1,6 +1,9 @@
 import random
 import math
 
+# If they say yes, output 'Let's get started...'
+# If they say no, output '*** How To Play ***'
+# If the answer is invalid, 'Please answer yes / no'
 def yes_no (question) :
 
     valid = False
@@ -20,20 +23,23 @@ def yes_no (question) :
             print ("Please answer yes / no")
             print ()
 
+# Instructions - how to play game
 def instructions() :
     print ()
     print ("**** How To Play ****")
     print ()
     print ("Welcome to Truth Or False - Maths edition")
+    print ()
     print ("This game will...") 
     print ()
-    print ("- Give you ")
-    print ("- The computer will then generate a series of maths addition questions")
+    print ("- First ask you how many questions of true or false you would like to play")
+    print ("- Then the computer will then generate a series of addition only math questions from 1 - 100")
     print ("- Your objective is to guess if the question is either true or false")
     print ("- Enter true / false or t / f")
     print ()
     print ("*** Good luck! ***")
 
+# Statement generator 
 def statement_generator (statement, decoration) :
 
     sides = decoration * 3
@@ -47,6 +53,27 @@ def statement_generator (statement, decoration) :
 
     return ""
 
+# Checks user enter an integer that is more than 0
+def check_rounds ():
+    while True:
+        response = input ("How many questions would you like: ")
+
+        round_error = "Please type either <enter> or an integer that is more than 0"
+        if response != "":
+            try:
+                response = int (response)
+
+                if response < 1:
+                    print (round_error)
+                    continue
+
+            except ValueError:
+                print (round_error)
+                continue
+
+        return response
+
+# Choice checker
 def choice_checker (question, valid_list, error):
 
     valid = False
@@ -66,38 +93,20 @@ def choice_checker (question, valid_list, error):
         # output error if item not in list 
         print (error)
         print ()
-            
-def check_rounds ():
-    while True:
-        response = input ("How many rounds: ")
-
-        round_error = "Please type either <enter> or an integer that is more than 0"
-        if response != "":
-            try:
-                response = int (response)
-
-                if response < 1:
-                    print (round_error)
-                    continue
-
-            except ValueError:
-                print (round_error)
-                continue
-
-        return response
 
 # Main routine goes here...
+
+game_summary = [] 
+game_guess_history = []
 
 rounds_lost = 0
 rounds_played = 0
 rounds_played = 0
 
-GUESSES_ALLOWED = 5
-
-mode = "regular"
-
 statement_generator ("Welcome to Truth Or False - Maths edition", "*")
 print ()
+
+# asks user if they have played, if no displays introduction, if yes game continues
 
 played_before = yes_no ("Have you played the game before? ")
 if played_before == "no":
@@ -105,18 +114,73 @@ if played_before == "no":
 
 print ()
 print ("Let's get started...")
+print ()
 
-print (check_rounds)
+# Main routine goes here...
 
+rounds_played = 0
+choose_instruction = "True Or False: "
+user = check_rounds
 
+guess = ""
 
+# Ask user for # of rounds and check if valid, <enter> for infinite mode
+rounds = check_rounds ()
 
+end_game = "no"
+while end_game == "no":
 
+    # Start of Game Play Loop
 
+    # question generator, randomizes equations between 1 - 20
 
+    num1 = random.randint(1, 20)
+    num2 = random.randint(1, 20)
+    num3 = random.randint(1, 20)
 
+    print()
+    question = "{} + {}".format(num1, num2)
 
+    actual_answer = num1 + num2
+    false_answer = actual_answer + num3
 
+    true_question = eval("{} == {}".format(actual_answer, question))
+    ("True", true_question)
 
+    false_question = eval("{} == {}".format(actual_answer, num3))
+    ("False", false_question)
 
+    # Rounds Heading (shows round for continuous or round / of total rounds for regular mode)
 
+    if rounds == "":
+        heading = "Continiuous Mode: Round {}".format (rounds_played + 1)
+    else:
+        heading = "Question {} of {}:".format (rounds_played +1, rounds)
+
+    print (heading)
+    print ()
+
+    # Generating true questions with correct answers
+    # If user enters true and answer is true, prints "well done!"
+    # If user enters anything other than true, prints "Sorry the correct answer was true"
+
+    answer = input("{} = {} - True or False: ".format (question, actual_answer))
+
+    if answer == 'true' or answer == 't': 
+        print ()
+        print("Well done! The answer was true :)")
+
+    else:
+        print ()
+        print("Sorry, the correct answer was true :(")
+
+    # End game if exit code is typed
+    if guess == "xxx":
+        break
+
+    rounds_played += 1
+
+    if rounds_played == rounds:
+        end_game = "yes"
+
+    # **** rest of loop / game *****
